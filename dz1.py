73690ecbf5fa3cb7c510ef1f6afc92e1f99c4711
alexeyprimachenko <33811743+alexeyprimachenko@users.noreply.github.com>
2022-01-12 16:53:14 +0200

    Add files via upload
    First HW in Hillel

---
+++ dz1.py
@@ -0,0 +1,67 @@
+
+#2
+int1 = 10 
+int2 = 30
+ 
+res = int1 + int2
+print(res)
+ 
+int1 = 10 
+int2 = 30
+ 
+res1 = int1 - int2
+print(res1)
+ 
+int1 = 10 
+int2 = 30
+ 
+res2 = int1 * int2
+print(res2)
+ 
+int1 = 10 
+int2 = 30
+ 
+res3 = int1 / int2
+print(res3)
+ 
+#3 
+my_bool = res < res1
+print(my_bool)
+ 
+my_bool = res > res1
+print(my_bool)
+
+my_bool = res == res1
+print(my_bool)
+
+my_bool = res != res1
+print(my_bool)
+
+my_bool = res1 < res2
+print(my_bool)
+ 
+my_bool = res1 > res2
+print(my_bool)
+
+my_bool = res1 == res2
+print(my_bool)
+
+my_bool = res1 != res2
+print(my_bool)
+
+my_bool = res2 < res3
+print(my_bool)
+ 
+my_bool = res2 > res3
+print(my_bool)
+
+my_bool = res2 == res3
+print(my_bool)
+
+my_bool = res2 != res3
+print(my_bool)
+
+#4
+str1="Hello " 
+str2="world"
+print(str1 + str2)
