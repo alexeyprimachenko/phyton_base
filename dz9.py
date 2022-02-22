eb0e24fdfa72f4c7bb73c09698ff9b9fa5456ddc
alexeyprimachenko <33811743+alexeyprimachenko@users.noreply.github.com>
2022-02-22 22:59:32 +0200

    Add files via upload
    Ninth HW in Hillel

---
+++ dz9.py
@@ -0,0 +1,43 @@
+# Создайте класс "Транспортное средство" и отнаследуйтесь от него классами "Самолет", "Автомобиль", "Корабль". 
+# Продумайте и реализуйте в классе "Транспортное средство" общие атрибуты для "Самолет", "Автомобиль", "Корабль". 
+# В наследниках реализуйте характерные для них атрибуты и методы
+
+class Vehicle:
+    def moveVehicle(self):
+        print(corpus,engines)
+
+corpus="All vehicles has corpuses"
+engines="and all vehicles has engines"
+vehicle=Vehicle()
+Vehicle.moveVehicle(vehicle)
+
+class Airplane:
+    def airPlane(self):
+        print(namePlane)
+
+namePlane="Airplane"
+
+
+plane=Airplane()
+Airplane.airPlane(plane)
+
+class Auto:
+    def carAuto(self):
+        print(nameAuto)
+
+nameAuto="Auto"
+
+
+auto=Auto()
+Auto.carAuto(auto)
+
+class Ship:
+    def shipShip(self):
+        print(nameShip)
+
+nameShip="Ship"
+
+
+ship=Ship()
+Ship.shipShip(ship)
+
